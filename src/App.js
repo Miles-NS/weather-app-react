dc71ef12ce93c42b41c4b8e4b2a1b4eabaf6f175
Miles-NS <milesnosi@gmail.com>
2023-11-15 04:46:16 +0200

    added a source code link

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
         <form>
           <input type="search" placeholder="type city.." />
           <input type="submit" value="search" />
+          <p>
+            <a href="https://github.com/Miles-NS/weather-app-react.git">
+              open-source code
+            </a>{" "}
+            by Nosipho Miles
+          </p>
         </form>
       </header>
     </div>
